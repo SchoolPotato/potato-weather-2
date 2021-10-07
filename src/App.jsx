9020c14fbbf569cfca26707f3aa2154ccdbe9a77
Alexander Welton <71471789+SchoolPotato@users.noreply.github.com>
2021-10-07 15:20:36 -0700

    Added more charts, forecasts, and began work on footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Graph from './Components/Graph';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Graph />
+      <Footer />
     </div>
   );
 }
--- src/Components/Graph.js
+++ src/Components/Graph.js
@@ -22,7 +22,7 @@ const Graph = () => {
                 (error) => {
                     setIsLoading(false);
                     setIsError(true);
-                    console.log(error);
+                    // console.log(error);
                 }
             )
     }, [])
@@ -32,7 +32,7 @@ const Graph = () => {
         for(let i = 0; i < weather.properties.periods.length; i+=2){
             (i !== 0) ? string = string + ",'" + weather.properties.periods[i].name + "'" : string = string + "'" + weather.properties.periods[i].name + "'";
         }
-        console.log(string);
+        // console.log(string);
         return string;
     }
 
@@ -41,7 +41,7 @@ const Graph = () => {
         for(let i = 0; i < weather.properties.periods.length; i+=2){
             (i !== 0) ? string = string + "," + weather.properties.periods[i].temperature : string = string + weather.properties.periods[i].temperature; 
         }
-        console.log(string);
+        // console.log(string);
         return string;
     }
 
@@ -50,17 +50,31 @@ const Graph = () => {
         for(let i = 1; i < weather.properties.periods.length; i+=2){
             (i !== 1) ? string = string + "," + weather.properties.periods[i].temperature : string = string + weather.properties.periods[i].temperature; 
         }
-        console.log(string);
+        // console.log(string);
         return string;
     }
 
     const currentWeather = ()=>{
-        let string = "";
-        for(let i = 1; i < weather.properties.periods.length; i+=2){
-            (i !== 1) ? string = string + "," + weather.properties.periods[i].temperature : string = string + weather.properties.periods[i].temperature; 
+        let weatherTypesObject = {};
+        let weatherTypesCount = "";
+        let weatherTypes = "";
+
+        for(let i = 0; i<weather.properties.periods.length; i++){
+            let tempString = weather.properties.periods[i].shortForecast;
+            if (!weatherTypesObject[tempString]){
+                weatherTypesObject[tempString] = 0;
+            }
+            weatherTypesObject[tempString]++
         }
-        console.log(weather.properties.periods);
-        return string;
+        
+         for (let type in weatherTypesObject) {
+            weatherTypesCount = weatherTypesCount + weatherTypesObject[type] + ",";
+            weatherTypes = weatherTypes + "'" + type + "',";
+        }
+
+        weatherTypesCount = weatherTypesCount.substring(0, weatherTypesCount.length-1);
+        weatherTypes = weatherTypes.substring(0, weatherTypes.length-1);
+        return [weatherTypes, weatherTypesCount, weatherTypesObject];
     }
 
     if(isError){
@@ -78,9 +92,21 @@ const Graph = () => {
                 {/*Days*/}
                 <img id="graph" src={`https://quickchart.io/chart?c={type:'line',data:{labels:[${currentLabels()}],datasets:[{label:'Nights',data:[${followingData()}]},{label: 'Days',data:[${currentData()}]}]}}`} alt=""></img>
 
-                <br />
+                <hr />
                 <h1 id="header">Weather:</h1>
-                <img id="graph" src={`https://quickchart.io/chart?c={type:'pie',data:{labels:[${currentLabels()}], datasets:[{data:[${currentWeather()}], borderWidth:0, backgroundColor:['rgba(9, 53, 121, 1)','rgba(0, 86, 154, 1)','rgba(1, 116, 179, 1)','rgba(0, 147, 205, 1)','rgba(0, 165, 220, 1)','rgba(0, 212, 255, 1)','rgba(100, 231, 255, 1)']}]}}`} alt=""></img><br />
+                <div id="info">
+                    <div id="forecasts">
+                        {weather.properties.periods.map((item)=>{
+                            let forecast = "forecast"
+                            if(item.number % 2 !== 0){
+                                forecast += " zebra";
+                            }
+                            return <h3 key={item.number} id={item.number} className={forecast} >{item.name}: {item.shortForecast}</h3>
+                        })}
+                    </div>
+                    <img id="graph2" src={`https://quickchart.io/chart?c={type:'pie',data:{labels:[${currentWeather()[0]}], datasets:[{data:[${currentWeather()[1]}], borderWidth:0}]}}`} alt=""></img>
+                </div>
+                <br />
 
 
                 {/*Nights
